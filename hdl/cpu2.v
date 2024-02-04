2fff6de2e6c7afcb8fbad9bacd0b6d480eb54ab0
Ico Doornekamp <ico@zevv.nl>
2024-02-04 21:27:48 +0100

    cpu2

--- hdl/cpu2.v
+++ hdl/cpu2.v
@@ -1,423 +1,357 @@
-
 // https://github.com/jameslzhu/riscv-card/blob/master/riscv-card.pdf
 // https://riscv.org/wp-content/uploads/2017/05/riscv-spec-v2.2.pdf
 
 `include "alu.v"
+`include "common.v"
+`include "regs.v"
 
-module cpu(
+module cpu
+#(
+   parameter W = 32
+)
+(
    input clk,
    output reg ren, output reg [15:0] addr, input [31:0] rdata, input rd_valid,
-   output reg wen, output reg [31:0] wdata, output reg[3:0] wmask,
+   output reg wen, output reg [W-1:0] wdata, output reg[3:0] wmask,
    output reg debug
 );
    
    localparam
-      OP_ALU_R = 7'b0110011,
-      OP_ALU_I = 7'b0010011,
-      OP_LOAD = 7'b0000011,
-      OP_STORE = 7'b0100011,
-      OP_BRANCH = 7'b1100011,
-      OP_JAL = 7'b1101111,
-      OP_JALR = 7'b1100111,
-      OP_LUI = 7'b0110111,
-      OP_AUIPC = 7'b0010111;
-
-   localparam
-      BOOT = 0,
-      FETCH = 1,
-      BRANCH = 2,
-      LOAD_WAIT = 3,
-      ST_AUIPC = 4,
-      LD_RS1 = 5,
-      LD_RS2_STORE = 6,
-      LOAD = 7,
-      LD_INST = 8,
-      LD_PC = 9,
-      LD_SP = 10,
-      ST_SP = 11,
-      ST_JAL = 12,
-      ST_JALR = 13,
-      ST_ALU = 14,
-      ST_LOAD = 15,
-      ST_LUI = 16,
-      ST_STORE = 17,
-      FAULT = 31;
-
-   localparam
-      BR_BEQ = 3'h0,
-      BR_BNE = 3'h1,
-      BR_BLT = 3'h4,
-      BR_BGE = 3'h5,
-      BR_BLTU = 3'h6,
-      BR_BGEU = 3'h7;
-
-   localparam
-      VEC_RESET = 15'h0080,
-      VEC_SP    = 15'h0084;
-
-
-   initial begin
-      ren = 0;
-      wen = 0;
-   end
+      VEC_RESET = 16'h0000,
+      VEC_SP    = 16'h0004;
 
    // CPU state
-   reg [4:0] state = 0;
+   reg [4:0] state = `ST_BOOT;
    reg [15:0] pc = 0;
-   reg [31:0] rd_val = 0;
-   reg [31:0] rs1_val = 0;
-   reg [31:0] rs2_val = 0;
-   reg alu_in2_rs2;
-   wire fetch = (state == FETCH);
+   
+   // Instruction decoding
+   reg [6:0] opcode = 0;
+   reg [4:0] rd = 0;
+   reg [6:0] funct7 = 0;
+   reg [2:0] funct3 = 0;
+   reg [4:0] rs1 = 0;
+   reg [4:0] rs2 = 0;
+   reg [W-1:0] imm = 0;
 
-   // Decoded instruction
-   reg [6:0] opcode;
-   reg [4:0] rd;
-   reg [6:0] funct7;
-   reg [2:0] funct3;
-   reg [4:0] rs1;
-   reg [4:0] rs2;
-   reg signed [31:0] imm = 0;
-  
-   // ALU
-   wire [31:0] alu_in1 = rs1_val;
-   reg [31:0] alu_in2;
-   reg [3:0] alu_fn;
-   wire [31:0] alu_out;
-   wire alu_zero;
-
-   always @(*) begin
-      alu_in2 = (alu_in2_rs2) ? rdata : imm;
+   always @(posedge clk)
+   begin
+      if(state == `ST_F_SP) begin
+         rd = 2;
+      end
+      if(state == `ST_DECODE) begin
+         opcode <= rdata[6:0];
+         rd <= rdata[11:7];
+         funct7 <= rdata[31:25];
+         funct3 <= rdata[14:12];
+         rs1 <= rdata[19:15];
+         rs2 <= rdata[24:20];
+         case (rdata[6:0])
+            `OP_ALU_I:  imm <= { {20{rdata[31]}}, rdata[31:20] };
+            `OP_LOAD:   imm <= { {20{rdata[31]}}, rdata[31:20] };
+            `OP_STORE:  imm <= { {20{rdata[31]}}, rdata[31:25], rdata[11:7] };
+            `OP_BRANCH: imm <= { {19{rdata[31]}}, rdata[31], rdata[7], rdata[30:25], rdata[11:8], 1'b0};
+            `OP_JAL:    imm <= { rdata[31], rdata[31], rdata[19:12], rdata[20], rdata[30:21], 1'b0};
+            `OP_JALR:   imm <= { {20{rdata[31]}}, rdata[31:20] };
+            `OP_LUI:    imm <= { rdata[31:12], 12'b0 };
+            `OP_AUIPC:  imm <= { rdata[31:12], 12'b0 };
+         endcase
+      end
    end
 
-   alu alu(
-      .x(alu_in1),
-      .y(alu_in2),
+   // Register file
+   reg reg_ren = 0;
+   reg reg_wen = 0;
+   wire [W-1:0] rs1_val;
+   wire [W-1:0] rs2_val;
+   reg [W-1:0] rd_val;
+
+   regs #(.W(W)) regs(
+      .clk(clk),
+      .ren(reg_ren),
+      .rs1(rs1), .rs2(rs2), .rs1_val(rs1_val), .rs2_val(rs2_val),
+      .wen(reg_wen), .rd(rd), .rd_val(rd_val)
+   );
+
+   // ALU
+   reg [W-1:0] alu_x;
+   reg [W-1:0] alu_y;
+   reg [3:0] alu_fn;
+   wire [W-1:0] alu_out;
+   wire alu_zero;
+
+   alu #(.W(W)) alu(
+      .x(alu_x),
+      .y(alu_y),
       .fn(alu_fn),
       .out(alu_out),
       .zero(alu_zero)
    );
 
-   reg [31:0] inst = 0;
+   reg [1:0] alu_x_sel;
+   reg [1:0] alu_y_sel;
 
+   // ALU control
    always @(*) begin
-      opcode = inst[6:0];
-      rd = inst[11:7];
-      funct7 = inst[31:25];
-      funct3 = inst[14:12];
-      rs1 = state == LD_INST ? rdata[19:15] : inst[19:15];
-      rs2 = inst[24:20];
-   end
+      alu_fn = `ALU_FN_ADD;
+      alu_x_sel = `ALU_X_RS1;
+      alu_y_sel = `ALU_Y_IMM;
 
-   always @(*) begin
-      alu_in2_rs2 = 0;
-      alu_fn = 0;
-      case (inst[6:0])
-         OP_ALU_R: begin
-            alu_in2_rs2 = 1;
+      case (opcode)
+         `OP_ALU_R: begin
             alu_fn = { funct7[5], funct3 };
+            alu_y_sel = `ALU_Y_RS2;
          end
-         OP_ALU_I: begin
+         `OP_ALU_I: begin
             alu_fn = { (funct3 == 3'h1 || funct3 == 3'h5) ? imm[10] : 1'b0, funct3 };
          end
-         OP_LOAD: begin
-            alu_fn = 4'h0; // ADD
-         end
-         OP_STORE: begin
-            alu_fn = 4'h0; // ADD
-         end
-         OP_BRANCH: begin
-            alu_in2_rs2 = 1;
+         `OP_BRANCH: begin
             case (funct3)
-               BR_BEQ: alu_fn = 4'h8;
-               BR_BNE: alu_fn = 4'h8;
-               BR_BLT: alu_fn = 4'h2;
-               BR_BGE: alu_fn = 4'h2;
-               BR_BLTU: alu_fn = 4'h3;
-               BR_BGEU: alu_fn = 4'h3;
+               `BR_BEQ,  `BR_BNE:  alu_fn = `ALU_FN_SUB;
+               `BR_BLT,  `BR_BGE:  alu_fn = `ALU_FN_LT;
+               `BR_BLTU, `BR_BGEU: alu_fn = `ALU_FN_LTU;
             endcase
+            alu_y_sel = `ALU_Y_RS2;
+         end
+         `OP_JAL,
+         `OP_AUIPC: begin
+            alu_x_sel = `ALU_X_PC;
+         end
+         `OP_LUI: begin
+            alu_x_sel = `ALU_X_ZERO;
          end
       endcase
-   end
 
-   always @(*) begin
-      imm = 0;
-      case (inst[6:0])
-         OP_ALU_I: imm = { {20{inst[31]}}, inst[31:20] };
-         OP_LOAD: imm = { {20{inst[31]}}, inst[31:20] };
-         OP_STORE: imm = { {20{inst[31]}}, inst[31:25], inst[11:7] };
-         OP_BRANCH: imm = { {19{inst[31]}}, inst[31], inst[7], inst[30:25], inst[11:8], 1'b0};
-         OP_JAL: imm = { inst[31], inst[31], inst[19:12], inst[20], inst[30:21], 1'b0};
-         OP_JALR: imm = { {20{inst[31]}}, inst[31:20] };
-         OP_LUI: imm = { inst[31:12], 12'b0 };
-         OP_AUIPC: imm = { inst[31:12], 12'b0 };
+      case (alu_x_sel)
+         `ALU_X_RS1: alu_x = rs1_val;
+         `ALU_X_PC: alu_x = pc;
+         `ALU_X_ZERO: alu_x = 32'd0;
+         default: alu_x = 32'd0;
+      endcase
+
+      case (alu_y_sel)
+         `ALU_Y_IMM: alu_y = imm;
+         `ALU_Y_RS2: alu_y = rs2_val;
+         `ALU_Y_FOUR: alu_y = 32'd4;
+         default: alu_y = imm;
       endcase
    end
- 
-   reg branch;
 
+   // pc + imm
+   wire [15:0] pc_plus_imm;
+   wire pc_plus_imm_carry;
+   adder #(16) add_pc_imm(pc, imm, 1'b0, pc_plus_imm, pc_plus_imm_carry);
+
+   // pc + 4
+   wire [15:0] pc_plus_4;
+   wire pc_plus_4_carry;
+   adder #(16) add_pc_4(pc, 32'd4, 1'b0, pc_plus_4, pc_plus_4_carry);
+
+   // Branch control
+   reg branch;
    always @(*) begin
       branch = 0;
       case (funct3)
-         BR_BEQ: if (alu_zero) branch = 1;
-         BR_BNE: if (!alu_zero) branch = 1;
-         BR_BLT,
-         BR_BLTU: if (alu_out) branch = 1;
-         BR_BGE,
-         BR_BGEU: if (!alu_out) branch = 1;
+         `BR_BEQ: if (alu_zero) branch = 1;
+         `BR_BNE: if (!alu_zero) branch = 1;
+         `BR_BLT,
+         `BR_BLTU: if (alu_out) branch = 1;
+         `BR_BGE,
+         `BR_BGEU: if (!alu_out) branch = 1;
       endcase
    end
 
-   // Memory load, handle unaligned and B/H/W
+   reg [W-1:0] load_val;
 
-   reg [31:0] rdata_shifted;
-   reg [31:0] load_cooked;
-
+   // Register read/write control
    always @(*) begin
-      case (addr[1:0])
-         2'h0: rdata_shifted = rdata[31:0];
-         2'h1: rdata_shifted = rdata[31:8];
-         2'h2: rdata_shifted = rdata[31:16];
-         2'h3: rdata_shifted = rdata[31:24];
+      reg_ren = 0;
+      reg_wen = 0;
+      rd_val = 0;
+      case (state)
+         `ST_F_SP:  begin
+            rd_val = rdata;
+            reg_wen = 1;
+         end
+         `ST_RD_REG: begin
+            reg_ren = 1;
+         end
+         `ST_X_LOAD_2: begin
+            rd_val = load_val;
+            reg_wen = 1;
+         end
+         `ST_WB_REG: begin
+            rd_val = alu_out;
+            reg_wen = 1;
+         end
+         `ST_X_JAL,
+         `ST_X_JALR: begin
+            rd_val = pc_plus_4;
+            reg_wen = 1;
+         end
       endcase
-      load_cooked = 0;
-      case (funct3)
-         3'h0: load_cooked = { {24{rdata_shifted[7]}}, rdata_shifted[7:0] };
-         3'h1: load_cooked = { {16{rdata_shifted[15]}}, rdata_shifted[15:0] };
-         3'h2: load_cooked = rdata_shifted;
-         3'h4: load_cooked = { 24'b0, rdata_shifted[7:0] };
-         3'h5: load_cooked = { 16'b0, rdata_shifted[15:0] };
-      endcase
+      if (rd == 0) reg_wen = 0;
    end
 
-   // Memory control
-
+   // Memory read/write control
    always @(*) begin
       ren = 0;
       wen = 0;
-      addr = -1;
+      addr = 0;
       wdata = 0;
       wmask = 4'b1111;
-
+      load_val = 0;
       case (state)
-         LD_PC: begin
-            addr = (!rd_valid) ? VEC_RESET : VEC_SP;
+         `ST_BOOT: begin
+            addr = VEC_SP;
             ren = 1;
          end
-         LD_SP: begin
+         `ST_F_PC: begin
+            addr = VEC_RESET;
+            ren = 1;
+         end
+         `ST_F_INST: begin
             addr = pc;
             ren = 1;
          end
-         ST_SP: begin
-            addr = (2 << 2);
-            wdata = rd_val;
-            wen = 1;
-         end
-         FETCH: begin
-            addr = pc;
-            ren = 1;
-         end
-         LD_INST: begin
-            addr = (!rd_valid) ? pc : (rs1 << 2);
-            ren = 1;
-         end
-         LD_RS1: begin
-            addr = (rs2 << 2);
-            ren = 1;
-         end
-         LD_RS2_STORE: begin
-            addr = (rs2 << 2);
-         end
-         BRANCH: begin
-            addr = (rs2 << 2);
-            ren = 1;
-         end
-         ST_STORE: begin
+         `ST_X_STORE: begin
             addr = alu_out;
-            case (funct3)
-               3'h0: wmask = 4'b1000;
-               3'h1: wmask = 4'b1100;
-               3'h2: wmask = 4'b1111;
+            case ({funct3[1:0], addr[1:0]})
+               4'b00_00: wmask = 4'b1000;
+               4'b00_01: wmask = 4'b0100;
+               4'b00_10: wmask = 4'b0010;
+               4'b00_11: wmask = 4'b0001;
+               4'b01_00: wmask = 4'b1100;
+               4'b01_10: wmask = 4'b0011;
+               4'b10_00: wmask = 4'b1111;
             endcase
-            case (addr[1:0])
-               2'h0: begin
-                  wdata = rs2_val;
-               end
-               2'h1: begin
-                  wdata = rs2_val << 8;
-                  wmask = wmask >> 1;
-               end
-               2'h2: begin
-                  wdata = rs2_val << 16;
-                  wmask = wmask >> 2;
-               end
-               2'h3: begin
-                  wdata = rs2_val << 24;
-                  wmask = wmask >> 3;
-               end
-            endcase
+            wdata = rs2_val << (addr[1:0] * 8);
             wen = 1;
          end
-         ST_LUI: begin
-            addr = (rd << 2);
-            wdata = imm;
-            wen = 1;
-         end
-         ST_LOAD: begin
-            addr = (rd << 2);
-            wdata = rd_val;
-            wen = 1;
-         end
-         ST_ALU: begin
-            addr = (rd << 2);
-            wdata = alu_out;
-            wen = 1;
-         end
-         ST_JAL,
-            ST_JALR: begin
-            addr = (rd << 2);
-            wdata = pc + 4;
-            wen = 1;
-         end
-         ST_AUIPC: begin
-            addr = (rd << 2);
-            wdata = pc + imm;
-            wen = 1;
-         end
-         LOAD: begin
-            addr = rs1_val + imm;
+         `ST_X_LOAD_1: begin
+            addr = alu_out;
             ren = 1;
          end
-
+         `ST_X_LOAD_2: begin
+            addr = alu_out;
+            case ({funct3[2:0], addr[1:0]})
+               5'b000_00: load_val = { {24{rdata[7]}}, rdata[7:0] };
+               5'b000_01: load_val = { {24{rdata[15]}}, rdata[15:8] };
+               5'b000_10: load_val = { {24{rdata[23]}}, rdata[23:16] };
+               5'b000_11: load_val = { {24{rdata[31]}}, rdata[31:24] };
+               5'b001_00: load_val = { {16{rdata[15]}}, rdata[15:0] };
+               5'b001_10: load_val = { {16{rdata[31]}}, rdata[31:16] };
+               5'b010_00: load_val = rdata;
+               5'b100_00: load_val = { 24'b0, rdata[7:0] };
+               5'b100_01: load_val = { 24'b0, rdata[15:8] };
+               5'b100_10: load_val = { 24'b0, rdata[23:16] };
+               5'b100_11: load_val = { 24'b0, rdata[31:24] };
+               5'b101_00: load_val = { 16'b0, rdata[15:0] };
+               5'b101_10: load_val = { 16'b0, rdata[31:16] };
+               default: load_val = 0;
+            endcase
+         end
       endcase
 
-      // Do not allow writes to address 0, the zero register
-      wen = (wen && addr != 0);
+   end
 
-      debug = (!(ren || wen));
+   always @(*) begin
+      debug = (ren || wen);
    end
 
    // CPU state machine
-
-   always @(posedge clk) begin
-
+   always @(posedge clk)
+   begin
       case (state)
-
-         BOOT: begin
-            pc <= pc + 4;
+         `ST_BOOT: begin
+            pc <= pc_plus_4;
             if (pc == 64) begin
                pc <= 0;
-               state = LD_PC;
+               state <= `ST_F_SP;
             end
          end
-
-         FETCH: begin
-            state <= LD_INST;
+         `ST_F_SP: begin
+            state <= `ST_S_SP;
          end
-
-         LD_INST: begin
+         `ST_S_SP: begin
+            state <= `ST_F_PC;
+         end
+         `ST_F_PC: begin
             if (rd_valid) begin
-               inst <= rdata;
-               case (rdata[6:0])
-                  OP_ALU_R: state <= LD_RS1;
-                  OP_ALU_I: state <= LD_RS1;
-                  OP_LOAD: state <= LD_RS1;
-                  OP_STORE: state <= LD_RS1;
-                  OP_BRANCH: state <= LD_RS1;
-                  OP_JAL: state <= ST_JAL;
-                  OP_JALR: state <= LD_RS1;
-                  OP_LUI: state <= ST_LUI;
-                  OP_AUIPC: state <= ST_AUIPC;
-                  default: state <= FAULT;
-               endcase
-            end
-         end
-
-         LD_RS1: begin
-            if (rd_valid) begin
-               rs1_val <= rdata;
-               state <= FAULT;
-               case (opcode)
-                  OP_ALU_R,
-                  OP_ALU_I: state <= ST_ALU;
-                  OP_STORE: state <= LD_RS2_STORE;
-                  OP_BRANCH: state <= BRANCH;
-                  OP_JALR: state <= ST_JALR;
-                  OP_LOAD: state <= LOAD_WAIT;
-               endcase
-            end
-         end
-
-         LD_RS2_STORE: begin
-            if (rd_valid) begin
-               rs2_val <= rdata;
-               state <= ST_STORE;
-            end
-         end
-         
-         LOAD_WAIT: begin
-            state <= LOAD;
-         end
-
-         BRANCH: begin
-            if (branch)
-               pc <= pc + imm;
-            else
-               pc <= pc + 4;
-            state <= FETCH;
-         end
-
-         LOAD: begin
-            if (rd_valid) begin
-               rd_val = load_cooked;
-               state <= ST_LOAD;
-            end
-         end
-
-         LD_PC: begin
-            if(rd_valid) begin
                pc <= rdata;
-               state <= LD_SP;
+               state <= `ST_F_INST;
             end
          end
-
-         LD_SP: begin
-            if(rd_valid) begin
-               rd_val <= rdata;
-               state <= ST_SP;
-            end
+         `ST_F_INST: begin
+            state <= `ST_DECODE;
          end
-
-         ST_ALU, ST_STORE, ST_LUI, ST_LOAD: begin
-            pc <= pc + 4;
-            state <= FETCH;
+         `ST_DECODE: begin
+            case (rdata[6:0])
+               `OP_ALU_R,
+               `OP_ALU_I,
+               `OP_STORE,
+               `OP_LOAD,
+               `OP_BRANCH,
+               `OP_JALR: state <= `ST_RD_REG;
+               `OP_LUI,
+               `OP_AUIPC: state <= `ST_WB_REG;
+               `OP_JAL: state <= `ST_X_JAL;
+               default: state <= `ST_FAULT;
+            endcase
          end
-
-         ST_SP: begin
-            state <= FETCH;
+         `ST_RD_REG: begin
+            case (opcode)
+               `OP_ALU_I,
+               `OP_ALU_R: state <= `ST_WB_REG;
+               `OP_LOAD: state <= `ST_X_LOAD_1;
+               `OP_JALR: state <= `ST_X_JALR;
+               `OP_STORE: state <= `ST_X_STORE;
+               `OP_BRANCH: state <= `ST_X_BRANCH;
+            endcase
          end
-
-         ST_AUIPC: begin
-            pc <= pc + 4;
-            state <= FETCH;
+         `ST_X_LOAD_1: begin
+            state <= `ST_X_LOAD_2;
          end
-         
-         ST_JAL: begin
-            pc <= pc + imm;
-            state <= FETCH;
+         `ST_WB_REG,
+         `ST_X_STORE,
+         `ST_X_LOAD_2: begin
+            pc <= pc_plus_4;
+            state <= `ST_F_INST;
          end
-         
-         ST_JALR: begin
-            pc <= rs1_val + imm;
-            state <= FETCH;
+         `ST_X_BRANCH: begin
+            if (branch)
+               pc <= pc_plus_imm;
+            else
+               pc <= pc_plus_4;
+            state <= `ST_F_INST;
          end
-
-         default: begin
-            state <= FAULT;
+         `ST_X_JAL,
+         `ST_X_JALR: begin
+            pc <= alu_out;
+            state <= `ST_F_INST;
          end
-
       endcase
    end
+
+`ifdef FORMAL
+
+   always @($global_clock)
+   begin
+      restrict(i_clk == !f_last_clk);
+      f_last_clk <= i_clk;
+   end
+
+
+   always @(*)
+   begin
+      on_ren_wen:
+         assert(!(ren && wen));
+
+      no_zero_write:
+         assert(!(addr == 0 && wen));
+
+   end
+
+`endif
 
 endmodule
 
